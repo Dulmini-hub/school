20fee7b584370eec01f9e0d3082ea7c39b30fda3
Dulmini <imdulmini@gmail.com>
2024-10-20 22:55:35 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <header>
+      <div className='header'>
+       </div>
+    </header>
+  );
+}
+
+export default Header;
